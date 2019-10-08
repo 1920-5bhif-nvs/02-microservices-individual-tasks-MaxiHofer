352d5a39c3de8940b392eef71611f95963f4c55a
MaxiHofer <maximilian@alexxdesign.at>
2019-10-08 15:53:40 +0200

    fixed project

--- Microservice/src/main/java/at/htl/MotorcycleRequest.java
+++ Microservice/src/main/java/at/htl/MotorcycleRequest.java
@@ -1,38 +1,19 @@
 package at.htl;
 
-import org.eclipse.microprofile.faulttolerance.Fallback;
-import org.eclipse.microprofile.faulttolerance.Retry;
-import org.eclipse.microprofile.metrics.MetricUnits;
-import org.eclipse.microprofile.metrics.annotation.Counted;
-import org.eclipse.microprofile.metrics.annotation.Timed;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import javax.inject.Inject;
-import javax.json.Json;
+import javax.json.JsonArray;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-@Path("/motorcycle")
-public class MotorcycleRequest {
 
-    @Inject
-    @RestClient
-    MotorcycleResource motorcycleResource;
+@RegisterRestClient
+public interface MotorcycleRequest {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Counted(name = "performedChecks", description = "How many rest requests have been performed.")
-    @Timed(name = "checksTimer", description = "A measure of how long it takes to perform the rest request", unit = MetricUnits.MILLISECONDS)
-    @Retry(maxRetries = 2)
-    @Fallback(fallbackMethod = "noMotorcycles")
-    public Response getMotorcycles() {
-        return Response.ok().entity(motorcycleResource.getMotorcycles()).build();
-    }
-
-    public Response noMotorcycles() {
-        return Response.ok().entity(Json.createArrayBuilder().build()).build();
-    }
+    @Path("/motorcycle/getMotorcycles")
+    JsonArray getMotorcycles();
 }
--- Microservice/src/main/java/at/htl/MotorcycleResource.java
+++ Microservice/src/main/java/at/htl/MotorcycleResource.java
@@ -1,20 +1,41 @@
 package at.htl;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.json.JsonArray;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@RegisterRestClient
-public interface MotorcycleResource {
+@Path("/motorcycle")
+public class MotorcycleResource {
+    @Inject
+    @RestClient
+    MotorcycleRequest motorcycleRequest;
 
     @GET
+    @Path("/getMotorcycles")
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/motorcycle")
-    JsonArray getMotorcycles();
+    @Counted(name = "requestAmount", description = "Amount of Requests")
+    @Timed(name = "responseTimer", description = "Response Time", unit = MetricUnits.MILLISECONDS)
+    @Retry(maxRetries = 2)
+    @Fallback(fallbackMethod = "noMotorcycles")
+    public Response getMotorcycles() {
+        return Response.ok().entity(motorcycleRequest.getMotorcycles()).build();
+    }
+
+    public Response noMotorcycles() {
+        return Response.ok().entity(Json.createArrayBuilder().build()).build();
+    }
 }
